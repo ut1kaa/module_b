ecf6c8668a38e36d09641f2b6b12c61ff967a248
Misha <utika.nice@gmail.com>
2024-10-11 00:00:15 +0300

    add migration

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -1,24 +1,27 @@
-from logging.config import fileConfig
+from src.models import Base
+from src.models.user import User
+from src.models.file import File
+from src.models.fileAccess import FileAccess
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+import asyncio
+from logging.config import fileConfig
 
 from alembic import context
 
+from src.database import async_engine
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -26,7 +29,14 @@ target_metadata = None
 # ... etc.
 
 
-def run_migrations_offline() -> None:
+def include_object(obj, name, type_, reflected, compare_to):
+    if obj.info.get("skip_autogen", False):
+        return False
+
+    return True
+
+
+def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -44,35 +54,47 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        # MATERIALIZED VIEW など無視する場合は下記をクラス属性に設定する
+        # __table_args__ = {"info": {"skip_autogen": True}}
+        include_object=include_object,
+        # 型変更を検知する
+        compare_type=True,
     )
 
     with context.begin_transaction():
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        dialect_opts={"paramstyle": "named"},
+        # MATERIALIZED VIEW など無視する場合は下記をクラス属性に設定する
+        # __table_args__ = {"info": {"skip_autogen": True}}
+        include_object=include_object,
+        # 型変更を検知する
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = async_engine
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    run_migrations_online()
+    asyncio.run(run_migrations_online())
