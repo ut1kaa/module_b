fca05c7f2dda6af4a79bf56cf3a41e4f26942999
ut1ka <utika.nice@gmail.com>
2024-10-11 17:09:27 +0300

    add  router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthSignIn from './pages/AuthSignIn';
+import AuthSignUp from './pages/AuthSignUp';
+import Index from './pages/Index';
+import PageFiles from './pages/PageFiles';
+import PrivateRoute from './components/PrivateRoute';
+import { isTokenValid } from './components/TokenValidator';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const token = localStorage.getItem('token');
+  const isTokenValidClientSide = isTokenValid(token);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={!isTokenValidClientSide ? <AuthSignIn />: <Navigate to="/"/>} />
+        <Route path="/register" element={!isTokenValidClientSide ? <AuthSignUp />: <Navigate to="/"/>} />
+        <Route path="/" element={<PrivateRoute><Index /></PrivateRoute>} />
+        <Route path="/files" element={<PrivateRoute><PageFiles /></PrivateRoute>} />
+      </Routes>
+    </Router>
   )
 }
 
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import "./styles/backend.css?v=1.0.0"
+import "./styles/line-awesome.min.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
